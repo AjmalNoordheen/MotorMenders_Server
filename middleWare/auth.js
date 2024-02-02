b59ee21a1ce475e4ead5a1d56bd1c7c9c492945c
Ajmal Noordheen <122587069+AjmalNoordheen@users.noreply.github.com>
2024-02-02 11:17:08 +0530

    Update auth.js

--- middleWare/auth.js
+++ middleWare/auth.js
@@ -2,27 +2,6 @@ const jwt = require('jsonwebtoken')
 const env = require('dotenv').config()
 
 
-// const verifyToken = async (req, res, next) => {
-//     let token    = req.header('Authorization')
-//     const {data} = req.query.data?req.query.data:''
-//     try {
-//         if (!token)return res.json({status:false,token:false, message: "Authentication failed: no token provided." });        
-
-//         if (token.startsWith("Bearer ")) {
-//             token = token.slice(7, token.length).trimLeft();
-//         }
-//         const verified = jwt.verify(token,process.env.jwtSecretKey);
-//         if(data==1 && verified){
-//             res.status(200)
-//         }else if(verified){
-//             next()
-//          }else{
-//             return res.json({status:false, message: "Authentication failed: invalid token." });
-//         }
-//     } catch (error) {
-//         return res.json({status:false, message: "Authentication failed: invalid token." });
-//     }
-// };
 
 const verifyToken = async (req, res, next) => {
     let token = req.header("Authorization");
